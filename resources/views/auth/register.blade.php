776591bc307fdf2ff753fdde9aae1ca558b73f5e
Steeven Orbe <stevenorbe416@gmail.com>
2022-03-31 14:52:47 -0500

    formulario registro editado

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,6 +12,16 @@
                         @csrf
 
                         <div class="row mb-3">
+                            <label for="text" class="col-md-4 col-form-label text-md-end">{{ __('Selecciona tu perfil') }}</label>
+                            <div class="col-md-6">
+                                <select class="form-select  mb-3" >
+                                <option value="secretaria">Secretaria</option>
+                                <option value="administrador">Administrador</option>
+                            <div>
+                        </select>
+                        </div>
+                        
+                        <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
