da9f332caa288056cbd271e0aa2d56ee599ff30f
JeffZeroc <jefferbonevalencia@gmail.com>
2022-05-24 00:03:56 -0500

    final5

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -32,7 +32,7 @@ class CreateUsersTable extends Migration
                 "rol" => "administrador",
                 "email" => "root@gmail.com",
                 "password" => Hash::make('123456789'),
-                "stado" => "Habilitado",
+                "estado" => "Habilitado",
             ]);
     }
 
